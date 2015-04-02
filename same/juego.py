c7be33a0117f7e590ff70dc3a140ea1a68c5d965
jeffrey.aguirre <j.jefry@gmail.com>
2015-04-01 22:28:25 -0500

    agregando funciones

--- same/juego.py
+++ same/juego.py
@@ -1,5 +1,12 @@
 
 class same(object):
+    """
+           y  y  y
+        x [1, 3, 1]
+        x [1, 3, 1]
+        x [1, 3, 1]
+    """
+    position = [(0, 1), (2, 1), (1, 2), (1, 0)]
     data = []
 
     def load_file(self, file):
@@ -13,13 +20,42 @@ class same(object):
                 print element, ' ',
             print
 
+    def is_clickable(self, x, y):
+        """
+            Return if we can click that coordinate
+        """
+        return
+
     def click(self, x, y):
         # Jeffrey
         pass
 
     def normalize(self):
+        """
+            Return board in tuples
+        """
         # Johan
         pass
 
-    def group():
-        pass
+    def is_done(self):
+        for row in self.data:
+            for elem in row:
+                if not elem == 0:
+                    return False
+        return True
+
+    def next_click(self, x, y):
+        """
+            Return the next click coordinate if is_Possible
+            otherwise None
+        """
+
+    def solve(self, path, x, y):
+        """
+            path = [((x, y), tuple(map(tuple, self.data)))
+        """
+        if self.is_done():
+            print 'solve'
+            return
+
+
