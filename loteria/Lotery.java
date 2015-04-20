f2da3e562c2399ebd0532c4564b833d360b273e3
johann2357 <johann2357@gmail.com>
2015-04-20 15:06:01 -0500

    adding lottery chooser

--- loteria/Lotery.java
+++ loteria/Lotery.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lotery {
 	public static void main(String [] args) {
-	
+		String s;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter the lottery");
+		s = in.nextLine();
+
+		ChooserUtils utils = new ChooserUtils();
+		ArrayList<String> lottery = utils.get_lotery_options();
+		ParserUtils parser = new ParserUtils();
+		parser.parse_file(lottery.get(0));
+		parser.parse_file(lottery.get(1));
 	}
 }
